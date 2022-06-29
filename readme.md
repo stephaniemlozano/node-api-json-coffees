e7ecee1799147497215e245ea1f7368f433426da
jonathans199 <jso1919@gmail.com>
2022-06-29 17:03:27 -0400

    updated with read me

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Coffees Lab - Local API with JSON data
+
+Created with plain HTML / CSS / Javascript pulling menu items from API
+
+1. Clone Repo
+2. Get `#menu` from index
+3. Using Fetch populate all menu items from local API
+
+![](./screenshot.png)
+
+![](./screenshot2.png)
--- scripts.js
+++ scripts.js
@@ -1,23 +1,5 @@
-const menu = document.querySelector('.menu-items')
+// copy JSON DATA from https://api.sampleapis.com/coffee/hot 
+// create JSON file in local API with all Coffees
 
-// get data from local API, get JSON from here https://api.sampleapis.com/coffee/hot
+// fetch local API and render coffee list here
 
-fetch('https://api.sampleapis.com/coffee/hot')// it should fetch local fetch('http://localhost:4000')
-  .then(myRes => myRes.json()) // handling promise then formatting to json
-  .then(data => {
-    let html = ''
-
-    data.forEach(eachItem => {
-      let htmlItem = `<div class="menu-items__item">
-      <img src='${eachItem.image}'>
-      <div> 
-        <h3>${eachItem.id}. ${eachItem.title}</h3>
-        <p>${eachItem.description}</p>
-      </div>
-    </div>`
-
-      html += htmlItem
-    })
-    menu.innerHTML = html
-  })
-  .catch(err => console.error(err))
